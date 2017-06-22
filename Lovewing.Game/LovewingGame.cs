8ccaec915c2ecf55fd1f34730d4e164206093dda
Capuccino <chinodesuuu@gmail.com>
2017-06-22 05:30:00 +0000

    Initial memes

---
+++ Lovewing.Game/LovewingGame.cs
@@ -0,0 +1,15 @@
+// Copyright (c) 2007-2017 Clara.
+// Licensed under the MIT License
+
+using System;
+using osu.Framework.Desktop;
+using osu.Framework.Platform;
+using osu.Framework;
+
+namespace Lovewing 
+{
+    public class Lovewing
+    {
+        //we'do smth here later uwu
+    }
+}
