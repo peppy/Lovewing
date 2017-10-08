19623d850d56a1d917bae2eeead512ef6186da4e
Capuccino <chinodesuuu@gmail.com>
2017-10-08 22:06:55 +0800

    add new TestCase and shit the loop

---
+++ Lovewing.VisualTests/Tests/TestCaseLovewingButton.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2014-2017 Clara.
+// Licensed under Eclipse Public License 1.0
+
+using System;
+using Lovewing.Game.Graphics.UserInterface;
+using osu.Framework.Testing;
+using OpenTK.Graphics;
+using System.Collections.Generic;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using System.Linq;
+using Lovewing.Game.Screens.Main;
+using osu.Framework.Graphics.Shapes;
+using OpenTK;
+
+namespace Lovewing.VisualTests.Tests
+{
+    internal class TestCaseLovewingButton : TestCase
+    {
+        
+    }
+}
