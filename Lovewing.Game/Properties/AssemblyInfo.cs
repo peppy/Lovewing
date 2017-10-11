4fffdbb73743c2a540ec59443a7139678f407a68
Andreas <apap04@otakoapp.com>
2017-10-11 18:28:42 -0400

    Added copyright attribute in AssemblyInfo

--- Lovewing.Game/Properties/AssemblyInfo.cs
+++ Lovewing.Game/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Lovewing.Game")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 Clara")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
