f016ffcf83375a3f14e2ac67eb047a4175e06be2
noud02 <noud.kerver@hotmail.nl>
2017-10-15 14:11:17 +0200

    revert the newlines

--- Lovewing.Game/Graphics/LovewingColours.cs
+++ Lovewing.Game/Graphics/LovewingColours.cs
@@ -9,11 +9,14 @@ namespace Lovewing.Game.Graphics
     {
         public Color4 Yellow = new Color4(247, 209, 33, 255);
         public Color4 LightYellow = new Color4(243, 202, 78, 255);
+
         public Color4 Magenta = new Color4(240, 110, 170, 255);
         public Color4 LightMagenta = new Color4(253, 144, 194, 255);
+
         public Color4 DarkBlue = new Color4(0, 114, 188, 255);
         public Color4 Blue = new Color4(0, 174, 239, 255);
         public Color4 LightBlue = new Color4(109, 207, 246, 255);
+
         public Color4 White = new Color4(225, 225, 225, 255);
     }
 }
