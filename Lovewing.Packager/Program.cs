08316c645d4ce1a2d4030bdc89e81c879c399973
sr229 <aguilarralph13@gmail.com>
2017-10-18 17:31:23 +0800

    oof more todos cos I need to sleep rip my sleep sched

--- Lovewing.Packager/Program.cs
+++ Lovewing.Packager/Program.cs
@@ -1,4 +1,7 @@
-﻿using Squirrel;
+﻿// Copyright (c) 2017 Clara.
+// Licensed under the EPL-1.0 License
+
+using Squirrel;
 using Newtonsoft.Json;
 
 namespace Lovewing.Game.Packager
@@ -8,5 +11,10 @@ namespace Lovewing.Game.Packager
         private const string nuget_path = @"packages\NuGet.CommandLine.4.3.0\tools\NuGet.exe";
         private const string squirrel_path = @"packages\squirrel.windows.1.7.8\tools\Squirrel.exe";
         private const string msbuild_path = @"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe";
+
+        public static void Main(string[] args)
+        {
+            //TODO: add some shit here later.
+        }
     }
 }
