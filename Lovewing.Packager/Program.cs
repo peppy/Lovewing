afa9e851dae96c4d49cce11e65aaf95e34e6e961
sr229 <aguilarralph13@gmail.com>
2017-10-18 17:13:39 +0800

    add packager project for use later

---
+++ Lovewing.Packager/Program.cs
@@ -0,0 +1,12 @@
+﻿using Squirrel;
+using Newtonsoft.Json;
+
+namespace Lovewing.Game.Packager
+{
+    internal class Program
+    {
+        private const string nuget_path = @"packages\NuGet.CommandLine.4.3.0\tools\NuGet.exe";
+        private const string squirrel_path = @"packages\squirrel.windows.1.7.8\tools\Squirrel.exe";
+        private const string msbuild_path = @"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe";
+    }
+}
