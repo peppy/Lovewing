0df96af0dd575819fc322c2dbe429350c6ff2b37
sr229 <aguilarralph13@gmail.com>
2018-01-14 17:32:30 +0800

    Fix DiscordRPC library not being found

--- Lovewing.Game/Online/DiscordRpc.cs
+++ Lovewing.Game/Online/DiscordRpc.cs
@@ -67,20 +67,20 @@ namespace Lovewing.Game.Online
             Ignore = 2
         }
 
-        [DllImport("discord-rpc", EntryPoint = "Discord_Initialize", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libdiscord-rpc", EntryPoint = "Discord_Initialize", CallingConvention = CallingConvention.Cdecl)]
         public static extern void Initialize(string applicationId, ref EventHandlers handlers, bool autoRegister,
             string optionalSteamId);
 
-        [DllImport("discord-rpc", EntryPoint = "Discord_Shutdown", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libdiscord-rpc", EntryPoint = "Discord_Shutdown", CallingConvention = CallingConvention.Cdecl)]
         public static extern void Shutdown();
 
-        [DllImport("discord-rpc", EntryPoint = "Discord_RunCallbacks", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libdiscord-rpc", EntryPoint = "Discord_RunCallbacks", CallingConvention = CallingConvention.Cdecl)]
         public static extern void RunCallbacks();
 
-        [DllImport("discord-rpc", EntryPoint = "Discord_UpdatePresence", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libdiscord-rpc", EntryPoint = "Discord_UpdatePresence", CallingConvention = CallingConvention.Cdecl)]
         public static extern void UpdatePresence(ref RichPresence presence);
 
-        [DllImport("discord-rpc", EntryPoint = "Discord_Respond", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libdiscord-rpc", EntryPoint = "Discord_Respond", CallingConvention = CallingConvention.Cdecl)]
         public static extern void Respond(string userId, Reply reply);
     }
 }
