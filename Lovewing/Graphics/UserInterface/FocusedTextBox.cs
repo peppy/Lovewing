f400d3d673888a0594e3b22ca50e7de5e5d6c19d
noud02 <noud.kerver@protonmail.com>
2018-04-22 14:46:51 +0200

    add textboxes

---
+++ Lovewing/Graphics/UserInterface/FocusedTextBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using OpenTK.Input;
+using osu.Framework.Input;
+
+namespace Lovewing.Graphics.UserInterface
+{
+    public class FocusedTextBox : LovewingTextBox
+    {
+        private bool focus;
+
+        public Action Exit => null;
+        public bool HoldFocus
+        {
+            get => focus;
+            set
+            {
+                focus = value;
+                if (!focus && HasFocus)
+                    GetContainingInputManager().ChangeFocus(null);
+            }
+        }
+
+        protected override bool OnKeyDown(InputState state, KeyDownEventArgs args)
+        {
+            if (args.Key == Key.Escape)
+            {
+                if (Text.Length > 0)
+                    Text = string.Empty;
+                else
+                    Exit?.Invoke();
+
+                return true;
+            }
+
+            return base.OnKeyDown(state, args);
+        }
+
+        public override bool RequestsFocus => HoldFocus;
+    }
+}
---
+++ Lovewing/Graphics/UserInterface/LovewingTextBox.cs
@@ -0,0 +1,45 @@
+﻿using OpenTK.Graphics;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+
+namespace Lovewing.Graphics.UserInterface
+{
+    public class LovewingTextBox : TextBox
+    {
+        public Color4 CommitColour { get; set; }
+        public Color4 FocusedColour { get; set; }
+        public Color4 UnfocusedColour { get; set; }
+        public Color4 PlaceholderColour { get; set; }
+        public Color4 TextColour { get; set; }
+
+        protected override float LeftRightPadding => 10;
+
+        protected override Color4 BackgroundCommit => CommitColour;
+        protected override Color4 BackgroundFocused => FocusedColour;
+        protected override Color4 BackgroundUnfocused => UnfocusedColour;
+
+        protected override SpriteText CreatePlaceholder() => new SpriteText
+        {
+            Colour = PlaceholderColour,
+            Margin = new MarginPadding { Left = 2 }
+        };
+
+        public LovewingTextBox()
+        {
+            Height = 40;
+            TextContainer.Height = 0.5f;
+
+            Current.DisabledChanged += disabled => Alpha = disabled ? 0.3f : 1;
+        }
+
+        protected override Drawable GetDrawableCharacter(char c) => new SpriteText
+        {
+            Colour = TextColour,
+            Text = c.ToString(),
+            TextSize = CalculatedTextSize
+        };
+    }
+}
