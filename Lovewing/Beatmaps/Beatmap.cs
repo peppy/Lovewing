df6fe325c81a16efaf11a7793f8c6cf6e4ff1fd4
noud02 <noud.kerver@protonmail.com>
2018-04-22 10:09:21 +0200

    add back beatmap stuff

---
+++ Lovewing/Beatmaps/Beatmap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Lovewing.Beatmaps
+{
+    public class Beatmap
+    {
+        public string Title { get; set; } = @"Unknown";
+        public string Background { get; set; } = @"Backgrounds/game_default";
+        public string Cover { get; set; } = @"Covers/muse";
+        public string MusicFile { get; set; } = @"song.mp3";
+        public string Author { get; set; } = @"Unknown";
+        public double BPM { get; set; } = 192.0;
+        public double NoteSpeed { get; set; } = 1.0;
+        public uint Difficulity { get; set; } = 1;
+        public List<Note> Notes { get; set; } = new List<Note>();
+        public List<string> Artists { get; set; } = new List<string>();
+    }
+}
---
+++ Lovewing/Beatmaps/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace Lovewing.Beatmaps
+{
+    public struct Note
+    {
+        public double Time;
+        public uint Attribute;
+        public uint Level;
+        public uint Effect;
+        public double EffectValue;
+        public uint Position;
+    }
+}
